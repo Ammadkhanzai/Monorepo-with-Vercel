a1cb147cfd368665aec0b4b11ac79799794602b5
Muhammad Ali khan <alykhan2212@gmail.com>
2021-06-25 21:22:03 +0500

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,5 @@ module.exports = {
     env: {
         REACT_APP_API_URL: 'https://api-fileinstant.herokuapp.com',
     },
-    images: {
-        domains: ['api-fileinstant.herokuapp.com'],
-      },
   }
   
